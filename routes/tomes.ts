e1fc34f7ec1aca531ecb998d89c119700bb85c27
Antunes <36316038+zpdh@users.noreply.github.com>
2024-10-01 23:02:01 -0300

    fix: fix incorrect comparison method for position variable in tome endpoint

--- routes/tomes.ts
+++ routes/tomes.ts
@@ -37,7 +37,7 @@ tomeRouter.get("/tomes/:username", async (request: Request<{ username: string }>
             return;
         }
 
-        const position = (await TomeModel.find({ dateAdded: { $lt: result.dateAdded } }).countDocuments()) + 1;
+        const position = (await TomeModel.find({ dateAdded: { $lt: result.dateAdded.getTime() } }).countDocuments()) + 1;
 
         // Return 'OK' if nothing goes wrong
         response.status(200).send({ username: result.username, position: position });
